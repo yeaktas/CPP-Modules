00b205930c5fe1cc8a209f828a66f31f488af1f3
yeaktas <yunusemreaktas97@gmail.com>
2023-08-16 09:46:25 +0300

    day 02

--- CPP-Module-05/README.md
+++ CPP-Module-05/README.md
@@ -3,4 +3,8 @@
 
 ###Kaynak
 
+
+https://www.geeksforgeeks.org/exception-handling-c/
+
 https://www.tutorialspoint.com/cplusplus/cpp_exceptions_handling.htm#:~:text=A%20C%2B%2B%20exception%20is%20a,try%2C%20catch%2C%20and%20throw.
+
--- CPP-Module-05/ex00/Bureaucrat.hpp
+++ CPP-Module-05/ex00/Bureaucrat.hpp
@@ -4,25 +4,30 @@
 #include <string>
 
 class Bureaucrat {
-
     private:
         const std::string _name;
         int _grade;
 
     public:
         Bureaucrat();
+        Bureaucrat(std::string name, int grade);
         Bureaucrat(const Bureaucrat& copy);
         Bureaucrat &operator=(const Bureaucrat &copy);
         ~Bureaucrat();
-
-        void GradeTooHighException(int grade);
-        void GradeTooLowException(int grade);
 
         std::string getName(void);
         int getGrade(void);
 
         void upGrade(void);
         void downGrade(void);
-};
 
+        // Exceptions
+        class GradeTooLowException : public std::exception{
+            public:
+                virtual const char *what(void) const throw();
+        }
+	    class GradeTooLowException : public std::exception{
+		    public:
+			    virtual const char *what(void) const throw();
+};
 #endif
