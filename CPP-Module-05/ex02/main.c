3814a57f4d938976efee612489255a056eafe332
yaktas <yunusemreaktas97@gmail.com>
2023-10-03 04:39:50 +0300

    subject

---
+++ CPP-Module-05/ex02/main.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaktas <yaktas@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 00:41:27 by yaktas            #+#    #+#             */
+/*   Updated: 2023/10/03 00:42:11 by yaktas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "AForm.hpp"
+
+/* 
+	Subject açıklama:
+
+	Bu görev, bir abstract (soyut) sınıf olan "AForm" adlı temel bir sınıf oluşturmayı gerektiriyor. 
+	Bu temel sınıfın özelliklerinin özel (private) olması ve tüm alt sınıflarda bu özelliklere erişiminin sınırlanması gerekiyor. 
+	Ardından, aşağıdaki somut (concrete) sınıfları eklemeniz isteniyor:
+
+	ShrubberyCreationForm: Gerekli notlar: imza 145, yürütme 137
+	Bu sınıf, çalışma dizininde "<hedef>_shrubbery" adlı bir dosya oluşturmalı ve içine ASCII ağaçlar yazmalıdır.
+
+	RobotomyRequestForm: Gerekli notlar: imza 72, yürütme 45
+	Bu sınıf, matkap sesleri yapmalı ve ardından "<hedef>" isimli hedefin robotomize edildiğini (başarı oranı %50) veya başarısız olduğunu bildirmelidir.
+
+	PresidentialPardonForm: Gerekli notlar: imza 25, yürütme 5
+	Bu sınıf, "<hedef>" isimli hedefin Zaphod Beeblebrox tarafından affedildiğini bildirmelidir.
+
+	Her bir sınıfın yapıcı metoduna yalnızca bir parametre alması gerekmektedir: formun hedefini belirten bir parametre, 
+	örneğin "ev" şeklinde, evde çalı eklemek isterseniz.
+
+	Ayrıca, temel form sınıfına "execute(Bureaucrat const & executor) const" adlı bir üye fonksiyon eklemeniz ve 
+	bu fonksiyonun somut sınıfların işlemini gerçekleştiren bir işlevi çağırmanız gerekmektedir. 
+	Bu işlem sırasında formun imzalandığını ve formu yürütmeye çalışan bürokratın yeterli düzeyde not aldığını kontrol etmelisiniz. 
+	Aksi takdirde uygun bir istisna (exception) fırlatmalısınız. 
+	İhtiyaca bağlı olarak bu gereksinimleri her somut sınıfta veya temel sınıfta kontrol etmek ve işlemi gerçekleştirmek isteyebilirsiniz.
+
+	Son olarak, Bureaucrat sınıfına "executeForm(Form const & form)" adlı bir üye fonksiyon eklemelisiniz. 
+	Bu fonksiyon, formu yürütmeye çalışmalıdır. Başarılı bir şekilde yürütülürse, "<bürokrat> <form'u> yürüttü" gibi bir çıktı vermelidir. 
+	Başarısız olursa açık bir hata mesajı vermelidir. Tüm bu işlemleri test ederek her şeyin beklenildiği gibi çalıştığından emin olmalısınız.
+	<bureaucrat> executed <form>
+
+*/
+
+int main(){
+
+
+}
