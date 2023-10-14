73dc05c303f32ceb4d42676bc8627dbf72e7efea
yaktas <yunusemreaktas97@gmail.com>
2023-10-14 16:24:49 +0300

    ex00 finish

--- CPP-Module-08/ex00/easyfind.hpp
+++ CPP-Module-08/ex00/easyfind.hpp
@@ -13,7 +13,36 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <iostream>
 
+/* template<typename T> int easyfind(T cont, int n)
+{
+	for (int i = 0; i < (int)cont.size(); i++)
+	{
+		if(cont[i] == n)
+		{
+			std::cout << "Finded! " << n;
+			return cont[i];
+		}
+	}
+	std::cout << "Not finded! " << n;  
+	throw (n);
+	return (NULL);
+}  */
 
+template<typename T> int easyfind(T cont, int n)
+{
+
+	if (std::find(cont.begin(), cont.end(), n) != cont.end())
+	{
+		std::cout << "Finded! " << n << std::endl;
+		return n;
+	}
+	throw (n);
+	return (0);
+}  
 
 #endif
--- CPP-Module-08/ex00/main.cpp
+++ CPP-Module-08/ex00/main.cpp
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "easyfind.hpp"
+
 /*
     Subject açıklama:
 
@@ -17,10 +19,45 @@
 
     İlk parametre, T türünden bir veri yapısı (container) olmalıdır.
     İkinci parametre ise bir tam sayı (integer) olmalıdır.
-    Varsayılan olarak, T türünün bir tamsayıları içeren bir veri yapısı olduğu kabul edilmektedir. İşlevin amacı, ikinci parametreyi (verilen tam sayıyı) birinci parametre içinde aramaktır. Eğer ikinci parametreyi bulursa, işlev bu değeri döndürmelidir. Ancak eğer ikinci parametre verilen veri yapısında bulunmuyorsa, işlev ya bir istisna fırlatabilir ya da tercihinize göre belirlediğiniz bir hata değerini döndürebilir.
+    Varsayılan olarak, T türünün bir tamsayıları içeren bir veri yapısı olduğu kabul edilmektedir. 
+    İşlevin amacı, ikinci parametreyi (verilen tam sayıyı) birinci parametre içinde aramaktır. 
+    Eğer ikinci parametreyi bulursa, işlev bu değeri döndürmelidir. Ancak eğer ikinci parametre verilen veri yapısında bulunmuyorsa, 
+    işlev ya bir istisna fırlatabilir ya da tercihinize göre belirlediğiniz bir hata değerini döndürebilir.
 
-    Bu görevdeki ilham kaynağı, standart C++ konteynerlerinin (vector, list, set, map, vb.) nasıl davrandığını incelemektir. Bu işlevin işleyişini ve hata işlemi yöntemini kendi kodunuzda uygulamanız gerekiyor. Son olarak, işlevin doğru bir şekilde çalıştığını doğrulamak için kendi testlerinizi oluşturup işlevi test etmelisiniz.
+    Bu görevdeki ilham kaynağı, standart C++ konteynerlerinin (vector, list, set, map, vb.) nasıl davrandığını incelemektir. 
+    Bu işlevin işleyişini ve hata işlemi yöntemini kendi kodunuzda uygulamanız gerekiyor. 
+    Son olarak, işlevin doğru bir şekilde çalıştığını doğrulamak için kendi testlerinizi oluşturup işlevi test etmelisiniz.
 
     Görev, C++ programlama dilinde temel işlevlerin nasıl oluşturulduğunu anlamanıza yardımcı olacak bir başlangıç görevidir.
 */
 
+int main(void)
+{
+    try
+    {
+        std::vector <int> vec;
+        std::list <int> list;
+
+        vec.push_back(1);
+        vec.push_back(2);
+        vec.push_back(3);
+        vec.push_back(4);
+        vec.push_back(5);
+        vec.push_back(6);
+
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+        list.push_back(4);
+        list.push_back(5);
+        list.push_back(6);
+        easyfind(vec, 4);
+        easyfind(list, 8);
+    }
+    catch(const int e)
+    {
+        std::cerr << "Not finded! " << e << std::endl;
+    }
+    
+    
+}
