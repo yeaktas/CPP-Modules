e783364cb2452be978606d8d4ef41793165ac1d5
yeaktas <yunusemreaktas97@gmail.com>
2023-10-14 05:36:17 +0300

    day one

---
+++ CPP-Module-08/ex00/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaktas <yaktas@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/14 03:35:22 by yaktas            #+#    #+#             */
+/*   Updated: 2023/10/14 03:35:22 by yaktas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+    Subject açıklama:
+
+    Bu görev, C++ programlama dilinde bir işlev şablonu oluşturmanızı istiyor. Bu işlev, herhangi bir tür T'yi kabul eder ve iki parametre alır:
+
+    İlk parametre, T türünden bir veri yapısı (container) olmalıdır.
+    İkinci parametre ise bir tam sayı (integer) olmalıdır.
+    Varsayılan olarak, T türünün bir tamsayıları içeren bir veri yapısı olduğu kabul edilmektedir. İşlevin amacı, ikinci parametreyi (verilen tam sayıyı) birinci parametre içinde aramaktır. Eğer ikinci parametreyi bulursa, işlev bu değeri döndürmelidir. Ancak eğer ikinci parametre verilen veri yapısında bulunmuyorsa, işlev ya bir istisna fırlatabilir ya da tercihinize göre belirlediğiniz bir hata değerini döndürebilir.
+
+    Bu görevdeki ilham kaynağı, standart C++ konteynerlerinin (vector, list, set, map, vb.) nasıl davrandığını incelemektir. Bu işlevin işleyişini ve hata işlemi yöntemini kendi kodunuzda uygulamanız gerekiyor. Son olarak, işlevin doğru bir şekilde çalıştığını doğrulamak için kendi testlerinizi oluşturup işlevi test etmelisiniz.
+
+    Görev, C++ programlama dilinde temel işlevlerin nasıl oluşturulduğunu anlamanıza yardımcı olacak bir başlangıç görevidir.
+*/
+
